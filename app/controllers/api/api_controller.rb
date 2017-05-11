23c69de9888f754398f0c307dc2a90b648fb07e4
elisa.vita.13@gmail.com <elisa.vita.13@gmail.com>
2017-05-11 16:22:42 +0300

    create api_controller.rb / change routes.rb

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,8 @@
+# module Api
+#   class ApiController < ApplicationController
+#     def add_deleted_status
+#       #как найти текущую вакансию?
+#       new_status_job = Jobs.update(:deleted_status => true)
+#     end
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+#Как правильно прописать путь??
+  #Нужна ли вообще эта колонка в таблице компаний???
+  #match 'add_deleted_status', to: 'api#add_deleted_status', via: 'patch'
+
+  # namespace :v1, defaults: { format: :json } do
+  #   match 'add_deleted_status', to: 'v1#add_deleted_status', via: 'patch'
+  # end
+
   scope module: 'api' do
     namespace :v1 do
       resources :jobs
